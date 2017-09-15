f19662d9c674735c9e3fe2d6d0471fbd959c2c54
Kemal Yenilmez <kemalyen@gmail.com>
2017-09-15 15:00:41 +0300

    Module reload fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-      <div>Merhaba Dünyacı</div>
+      <div>Hello World</div>
     );
   }
 }
